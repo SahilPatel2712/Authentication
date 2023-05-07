c187e8c75f87ad828c705db217a91fe636564607
Sahil Patel <sahilp14323@gmail.com>
2023-05-07 19:06:20 +0530

    level-3

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 
+const md5 = require('md5');
 
 const app = express()
 
@@ -22,8 +23,6 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] })
 const User = new mongoose.model("Users", userSchema)
 
 
@@ -40,14 +39,14 @@ app.route("/login")
     })
     .post((req, res) => {
         const username = req.body.username
-        const password = req.body.password
+        const password = md5(req.body.password)
         User.findOne({ email: username }).then((foundUser) => {
-            // if (foundUser.password===password) {
-            //     res.render("secrets")
+            if (foundUser.password===password) {
+                res.render("secrets")
 
-            // }
-            console.log(foundUser);
-
+            }
+            // console.log(foundUser);
+       
 
         }).catch((err) => {
             console.log(err);
@@ -61,7 +60,7 @@ app.route("/register")
     .post((req, res) => {
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         })
         newUser.save().then(() => {
             res.render("secrets")
